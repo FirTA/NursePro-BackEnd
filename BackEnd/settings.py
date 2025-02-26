52fd8f9e950612ed977436ea3b098eacace1f92e
Firdauz Tri Anggoro <86070626+FirTA@users.noreply.github.com>
2025-02-26 13:41:26 +0700

    1

--- BackEnd/settings.py
+++ BackEnd/settings.py
@@ -77,7 +77,7 @@ AUTHENTICATION_BACKENDS = [
 
 # CORS settings
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = False  # Set to False in production
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:3000').split(',')
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost:3000').split(',')
 
